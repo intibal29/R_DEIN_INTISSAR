e3577b73c859f6f67856009a38b82dbb07b71961
intibal29 <117351402+intibal29@users.noreply.github.com>
2024-11-13 19:44:43 +0100

    javadoc

---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.intissar.r","l":"ForTemporizadorController"},{"p":"com.intissar.r","l":"Main"}];updateSearchResults();
